260d20a5a6790390abea36b268b360f30976f07e
David Hellwig <helldavi@isu.edu>
2024-01-22 21:24:28 -0700

    Removed some files we don't need

--- target/debug/MiniGrep.d
+++ target/debug/MiniGrep.d
@@ -1,1 +1,1 @@
-C:\Users\David\Documents\RustLearning\bro\MiniGrep\target\debug\MiniGrep.exe: C:\Users\David\Documents\RustLearning\bro\MiniGrep\src\main.rs
+C:\Users\David\Documents\RustLearning\bro\MiniGrep-\target\debug\MiniGrep.exe: C:\Users\David\Documents\RustLearning\bro\MiniGrep-\src\main.rs
